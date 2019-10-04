3f74759af97b3beb297d420447ab40ac29c27608
Leveque <rleveque@ptc.com>
2019-10-04 21:23:31 +0200

    v

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todolist',
-  templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  template:
+  `<h1>{{listTitle}}</h1>
+    <div [hidden]='todos.length'>Aucun élément</div>
+    <ul>
+      <li *ngFor='let todo of todos; let i =
+index; let odd = odd' [ngStyle]='{
+          "background-color": !!odd ? "gray" : "",
+          "font-style": "italic"
+        }'>
+      <hr *ngIf='i'/>
+        <a [href]='"http://google.fr/#q=" + todo'
+target='_blank'>{{todo}}</a>
+      </li>
+    </ul>
+    <form (submit)='createTodo()'>
+      <input type='text' name='todoLabel' [(ngModel)]='newTodo' />
+    </form>`,
 })
-export class TodolistComponent implements OnInit {
+export class TodolistComponent {
+  listTitle = 'Ma Todo List';
+  todos: string[] = [];
+  newTodo = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  createTodo() {
+    if (this.newTodo) {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    }
   }
-
 }
